5dad58c768a3241db10c34069bf869809b77bc26
Carl <125189035+CaSchaeffer@users.noreply.github.com>
2023-10-02 15:50:57 +0200

    Update JetreportsFormularCopyCat.py
    The Script creates a ASCIIDOC File with all the Formulas now

--- JetreportsFormularCopyCat.py
+++ JetreportsFormularCopyCat.py
@@ -5,8 +5,14 @@ import os
 # Directory where your Excel files are located
 excel_directory = "C:\\Users\\c.stegen\\Desktop"
 
-# Regulärer Ausdruck zum Erkennen von Jet Reports-Formeln (=NL, =NP, =NF)
-jet_reports_formel_pattern = re.compile(r'(?i)=\s*(?:NL|NP|NF)\s*\([^)]*\)')
+# Regular expression to recognize Jet Reports formulas (=NL, =NP, =NF)
+jet_reports_formula_pattern = re.compile(r'(?i)=\s*(?:NL|NP|NF)\s*\([^)]*\)')
+
+# AsciiDoc content for the formulas
+asciidoc_content = ""
+
+# Set to store unique formulas
+unique_formulas = set()
 
 # Iterate through files in the directory
 for filename in os.listdir(excel_directory):
@@ -14,22 +20,35 @@ for filename in os.listdir(excel_directory):
         excel_file = os.path.join(excel_directory, filename)
 
         try:
-            # Öffnen Sie die Excel-Datei
+            # Open the Excel file
             wb = openpyxl.load_workbook(excel_file)
+
+            # Get the category or purpose of the Excel file (you need to fill this in)
+            excel_category = "Fill in the category or purpose here"
+
+            # Add the Excel file name and category to AsciiDoc content
+            asciidoc_content += f"= Excel File: {filename}\n"
+            asciidoc_content += f"== Category/Purpose: {excel_category}\n\n"
 
             # Iterate through worksheets in the workbook
             for ws in wb.worksheets:
-                # Durchsuchen Sie das Arbeitsblatt nach Formeln und schreiben Sie sie in eine Textdatei
-                with open('jet_reports_formeln.txt', 'a') as text_datei:
-                    for row_num, row in enumerate(ws.iter_rows(values_only=True), start=1):
-                        for col_num, zelle in enumerate(row, start=1):
-                            if isinstance(zelle, str) and jet_reports_formel_pattern.search(zelle):
-                                text_datei.write(f"Formula found in Sheet: {ws.title}, Row: {row_num}, Column: {col_num}\n")
-                                text_datei.write(f"Formula: {zelle}\n\n")
+                # Search the worksheet for formulas and add them to the AsciiDoc content
+                asciidoc_content += f"=== Sheet: {ws.title}\n\n"
+                for row_num, row in enumerate(ws.iter_rows(values_only=True), start=1):
+                    for col_num, cell in enumerate(row, start=1):
+                        if isinstance(cell, str) and jet_reports_formula_pattern.search(cell):
+                            formula = f"Row: {row_num}, Column: {col_num}\nFormula: {cell}\n\n"
+                            if formula not in unique_formulas:
+                                asciidoc_content += formula
+                                unique_formulas.add(formula)
 
-            # Schließen Sie die Excel-Datei
+            # Close the Excel file
             wb.close()
         except Exception as e:
             print(f"Error processing {excel_file}: {e}")
 
-print("Done searching through all workbooks.")
+# Save the AsciiDoc content to a file
+with open('formulas.adoc', 'w') as asciidoc_file:
+    asciidoc_file.write(asciidoc_content)
+
+print("Done searching through all workbooks. Formulas saved in 'formulas.adoc'.")
